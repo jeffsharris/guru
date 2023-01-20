6b795a85a07147f8cd7e9c0321c983005ca3883c
Jeff Harris <jeffharris@openai.com>
2023-01-20 12:31:24 -0800

    Add capability to take a query and then output an ordered list of sentences that most nearly match the query

--- embedder.py
+++ embedder.py
@@ -64,7 +64,7 @@ def compute_doc_embeddings_sample(sentences: list[str], sampleSize: int) -> list
     """
 
     return [
-        (sentence, get_doc_embedding(sentence)) for sentence in sentences[:sampleSize]
+        (sentence, get_doc_embedding(sentence.replace("\n", " "))) for sentence in sentences[:sampleSize]
     ]
 
 def write_to_csv(filename: str, data: list[tuple[str, list[float]]]) -> None:
@@ -90,10 +90,37 @@ def read_from_csv(filename: str) -> list[tuple[str, list[float]]]:
             data.append((sentence, embedding))
     return data
 
+
+def vector_similarity(x: list[float], y: list[float]) -> float:
+    """
+    We could use cosine similarity or dot product to calculate the similarity between vectors.
+    In practice, we have found it makes little difference. 
+    """
+    return np.dot(np.array(x), np.array(y))
+
+def order_document_sections_by_query_similarity(query: str, contexts: list[tuple[str, list[float]]]) -> list[(float, int, str)]:
+    """
+    Find the query embedding for the supplied query, and compare it against all of the pre-calculated document embeddings
+    to find the most relevant sections. 
+    
+    Return the list of document sections, sorted by relevance in descending order.
+    """
+    query_embedding = get_query_embedding(query)
+    
+    document_similarities = sorted([
+        (vector_similarity(query_embedding, doc_embedding), index, sentence) for index, (sentence, doc_embedding) in enumerate(contexts)
+    ], reverse=True)
+    
+    return document_similarities
+
+
 # Call the function with the file path
-sentences = parse_text_file('transcriptions.txt')
+# sentences = parse_text_file('transcriptions.txt')
+# write_to_csv("embeddings.csv", compute_doc_embeddings_sample(sentences, 59))
 
-write_to_csv("embeddings.csv", compute_doc_embeddings_sample(sentences, 10))
+sentenceEmbeddings = read_from_csv("embeddings.csv")
 
-ret = read_from_csv("embeddings.csv")
-print(ret[5])
+document_similarities = order_document_sections_by_query_similarity("what's at the center of our heart", sentenceEmbeddings)
+
+for score, index, sentence in document_similarities:
+    print(str(score) + ": " + str(index) + ": " + sentence)
